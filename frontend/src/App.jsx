5c59b428ad1ffbad7f4acc19369bb4a41f4af9ec
Ajstyles316 <fabriciochoque11@gmail.com>
2025-07-02 10:33:00 -0400

    pestañas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,30 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
 
 import { CssBaseline, ThemeProvider } from '@mui/material';
-
-import { baselightTheme } from "./theme/DefaultColors";
+import { useEffect } from 'react';
 import { RouterProvider } from 'react-router';
 import router from "./routes/Router.js"
+import { routeTitleMap } from './layouts/full/sidebar/MenuItems';
+
+import { baselightTheme } from "./theme/DefaultColors";
 
 function App() {
   const theme = baselightTheme;
+
+  useEffect(() => {
+    // Suscribirse a los cambios de navegación
+    const unsubscribe = router.subscribe((state) => {
+      const pathname = state.location.pathname;
+      const title = routeTitleMap[pathname] || 'Activos Fijos';
+      document.title = title;
+    });
+
+    // Establecer el título inicial
+    const initialTitle = routeTitleMap[window.location.pathname] || 'Activos Fijos';
+    document.title = initialTitle;
+
+    return () => unsubscribe();
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
--- frontend/src/views/sample-page/SamplePage.js
+++ frontend/src/views/sample-page/SamplePage.js
@@ -6,7 +6,7 @@ import DashboardCard from '../../components/shared/DashboardCard';
 
 const SamplePage = () => {
   return (
-    <PageContainer title="Sample Page" description="this is Sample page">
+    <PageContainer title="Reportes" description="this is Sample page">
 
       <DashboardCard title="Sample Page">
         <Typography>This is a sample page</Typography>
