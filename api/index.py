23e12ca62690d9029a691655b2e9d28ee97984b4
Ajstyles316 <fabriciochoque11@gmail.com>
2025-09-26 18:31:32 -0400

    Fix Vercel configuration

--- api/index.py
+++
@@ -1,9 +0,0 @@
-from django.core.wsgi import get_wsgi_application
-import os
-import sys
-
-# Configurar Django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion_maquinaria.settings')
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-application = get_wsgi_application()
